314dd29388a533a100fea3f398e89c5a2c4f693d
NsemaX <nsemax@hotmail.com>
2017-04-14 16:45:21 +0300

    initial commit

---
+++ CustomErrorPageMVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomErrorPageMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string result = "";
+            List<CustomClass> newlist;
+
+            //newlist = Helper.FullList();
+            newlist = Helper.FullListError();
+            
+
+
+            if (newlist.Count() > 0)
+                result = "hello";
+
+            ViewBag.Message = result;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ CustomErrorPageMVC/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomErrorPageMVC
+{
+    public static class Helper
+    {
+        public static List<CustomClass> FullList()
+        {
+
+            List<CustomClass> result = new List<CustomClass>()
+        {
+            new CustomClass(){  Code = 1,  Description = "Jessica" },
+            new CustomClass(){ Code = 2, Description = "Mandy" }
+        };
+
+
+            return result;
+        }
+        public static List<CustomClass> FullListError()
+        {
+            return null;
+        }
+    }
+}
